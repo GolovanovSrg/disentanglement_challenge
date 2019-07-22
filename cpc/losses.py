fe688654116002910c25c1c05ef50c5f0f45a8ad
Sergey Golovanov <sergey_xg@mail.ru>
2019-07-22 11:35:09 +0300

    update loss

--- cpc/losses.py
+++ cpc/losses.py
@@ -3,17 +3,23 @@ import torch.nn as nn
 
 
 class CPCLoss(nn.Module):
-    def __init__(self, reduction='mean'):
+    def __init__(self, n_negative=25, reduction='mean'):
         super().__init__()
+        self.n_negative = n_negative
         self.criterion = nn.CrossEntropyLoss(reduction=reduction)
 
     def forward(self, predictions, targets):
         predictions = predictions.view(*predictions.shape[:2], -1)
         targets = targets.view(*targets.shape[:2], -1)
 
-        scores = torch.bmm(targets.permute((0, 2, 1)), predictions)
-        labels = torch.arange(scores.shape[-1], dtype=torch.long, device=scores.device)
-        labels = labels.unsqueeze(0).expand(scores.shape[0], -1)
+        scores = [(predictions * targets).sum(dim=1)]
+        for _ in range(self.n_negative):
+            batch_perm_idxs = torch.randperm(predictions.shape[0])
+            patch_perm_idxs = torch.randperm(predictions.shape[-1])
+            scores.append((predictions * targets[batch_perm_idxs, ...][..., patch_perm_idxs]).sum(dim=1))
+
+        scores = torch.stack(scores, dim=1)
+        labels = torch.zeros(scores.shape[0], scores.shape[-1], dtype=torch.long, device=scores.device)
 
         loss = self.criterion(scores, labels)
 
